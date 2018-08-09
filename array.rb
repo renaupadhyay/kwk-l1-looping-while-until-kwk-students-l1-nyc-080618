0eaec26ed514fb8ef7d24d8d721240aaa9fa0228
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-09 13:27:18 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,12 @@
+
+disney= ["little mermaid", "cinderella", "belle", "mulan", "aurora"]
+puts disney
+
+
+age= [17,18,13,16]
+puts age
+
+book= ["Lovely Bones", "The Hate U Gift", "Between the World and Me", "The Handmaid's Tale"]
+
+puts book.length
+
